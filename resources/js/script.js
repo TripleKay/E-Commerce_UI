2218d4b006e850cca33ba959d7e8e69d0e279ccc
TripleKay <triplekay.k3@gmail.com>
2022-06-11 23:24:29 +0630

    mobile nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,24 +1,45 @@
 $(document).ready(function() {
-    // $('.all-cat-btn').click(function(){
-    //     $('.all-cat-menu').toggleClass('show');
-    // });
-    // $('.cat-item').click(function(){
-    //     $('.cat-item').toggleClass('active');
-    // });
+    $('.mobile-menu').click(function(){
+        $('.header-3-container').animate({left: '0px'},300);
+        $('.mobile-close').animate({left: '90%'},400);
+        $('.header-3').animate({left: '0px'},500);
+    });
+    $('.mobile-close').click(function(){
+        $('.header-3-container').animate({left: '-800px'},500);
+        $('.mobile-close').animate({left: '-300px'},400);
+        $('.header-3').animate({left: '-800px'},300);
+    });
+    $('#category-section').waypoint(function(direction) {
+
+        if(direction == 'down') {
+            $('.header-2').addClass('fixed');
+        } else {
+            $('.header-2').removeClass('fixed'); 
+        }
+        
+    },{
+        offset: '150px'
+    })
     $('.product-container').owlCarousel({
         // margin:10,
         nav:true,
         dots: false,
         responsive:{
             0:{
-                items:1
+                items:2
             },
             600:{
                 items:3
             },
+            900:{
+                items:2
+            },
             1000:{
-                items:4,
+                items:3,
                 nav:true,
+            },
+            1200:{
+                items: 4
             }
         }
     })
